07a3938dea115da6ca19b0f991bfdf74ed09032f
Arix23 <arivalenzuela23@gmail.com>
2020-05-19 19:29:38 -0600

    asd
    asd

---
+++ src/Arista.java
@@ -0,0 +1,16 @@
+
+public class Arista {
+	private int valor;
+	
+	public Arista(int valor) {
+		this.valor = valor;
+	}
+	
+	public void setValor(int valor) {
+		this.valor = valor;
+	}
+	
+	public int getValor() {
+		return this.valor;
+	}
+}
--- src/Grafito.java
+++ src/Grafito.java
@@ -3,9 +3,14 @@ public class Grafito {
 	public static void main(String[] args) {
 		//PRIMERO CREAR VERTICES
 		//DESPUES METER LAS CONEXIONES
-		//LUEGO CREAR HASHTABLE
+		//LUEGO CREAR HASHTABLE DE VÉRTICES
+		//EL ALGORITMO RECIBE LA HASHTABLE DE VÉRTICES
 		
 
+	}
+	
+	public void EBMPAlgorithm() {
+		
 	}
 
 }
