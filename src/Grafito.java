bb209dd3afcf2d904f6c66794dd9b60b4f2114a3
nataly-8h <54066974+nataly-8h@users.noreply.github.com>
2020-05-20 19:40:56 -0500

    Shuffle Random Tag

--- src/Grafito.java
+++ src/Grafito.java
@@ -1,19 +1,17 @@
 import java.util.*;
 
 public class Grafito {
-
+	
 	public static void main(String[] args) {
 		//PRIMERO CREAR VERTICES
 		//DESPUES METER LAS CONEXIONES
 		//LUEGO CREAR HASHTABLE DE VERTICES
 		//EL ALGORITMO RECIBE LA HASHTABLE DE VERTICES
 
-
-
 		Scanner sc = new Scanner(System.in);
 		int numVertex = sc.nextInt();
 		HashMap<Integer,Vertex> Graph = new HashMap<Integer,Vertex>();
-		HashMap<ConjuntoNodo,Arista> aristasProblema = new HashMap<ConjuntoNodo, Arista>();
+		HashMap<ConjuntoNodo, Arista> aristasProblema = new HashMap<ConjuntoNodo, Arista>();
 
 		//Se agregan las vertices al grafo y se les asigna una llave
 		for(int i = 1; i <= numVertex; i++){
@@ -34,8 +32,8 @@ public class Grafito {
 				}
 			}
 		}
+		
 		sc.close();
-
 
 		//INCIDENCIA
 		for (Arista value : aristasProblema.values()) {
@@ -46,42 +44,34 @@ public class Grafito {
 		    	}
 		    }
 		}
-
-
-
-
-
-
+		
+		/*
+		 3
+		 2 3 2
+		 1 3
+		 1 1
+		 */
+		
+		LinkedList<Integer> intList = createList(aristasProblema.size());
+		System.out.println(RandomTag(intList).toString());
+		System.out.println(RandomTag(intList).toString());
+		System.out.println(RandomTag(intList).toString());
 	}
 
-	public static void randomTag(HashMap<Integer, Vertex> Graph) {
-		int [] randomTag = new int[Graph.size()];
-		boolean [] used = new boolean[Graph.size()];
-
-		for(int i = 0; i < randomTag.length; i++ ) {
-			Random rnd = new Random();
-			int random = rnd.nextInt(randomTag.length);
-			 if(!used[random]) {
-				 System.out.println( "ra: " + random);
-				 randomTag[i] = random;
-				 used[random] = true;
-			 }else {
-				 System.out.println("rau: " + random + " - ao  " + (random + 1 % randomTag.length));
-				/*while(used[random]) {
-					 random %= randomTag.length;
-				 }
-
-				 randomTag[random] = random;
-				 used[random] = true;*/
-			 }
-		}
-
-		System.out.println();
-		for(int i = 0; i < randomTag.length; i++ ) {
-			System.out.print(randomTag[i] + " ");
-		}
+	public static LinkedList<Integer> RandomTag(LinkedList<Integer> intList) {
+		LinkedList<Integer> shuffled = (LinkedList<Integer>) intList.clone();
+		Collections.shuffle(shuffled);
+		return shuffled;
 	}
-
+	
+	public static LinkedList<Integer> createList(int size) {
+		LinkedList<Integer> intList = new LinkedList<Integer>();
+		for(int i = 0; i < size; i ++) {
+			intList.add(i + 1);
+		}
+		return intList;
+	}
+	
 	public void EBMPAlgorithm() {
 
 	}
